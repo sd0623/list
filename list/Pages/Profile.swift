fc73d3cdfff25669b27398b232b67600cb0fe58b
sd0623 <deegutlasathwika@gmail.com>
2023-10-10 23:01:25 -0400

    log in and log out working

--- list/Pages/Profile.swift
+++ list/Pages/Profile.swift
@@ -17,10 +17,64 @@ struct Profile: View {
 //                Header(size: 30.0, height: 100)
 //                    .offset(y: -90)
                 
-                Text("Welcome to your account!")
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading Profile...")
+                }
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        // Info: Name, Email, Joined since
+        VStack(alignment: .leading){
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            
+            HStack {
+                Text("Joined: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+            
+        }
+        .padding()
+        
+        // Log out
+        Form {
+            ListButton(title: "Log Out", bgColor: .red){
+                viewModel.logout()
+            }
+        }
+        .padding()
+        
+        Spacer()
     }
 }
 
